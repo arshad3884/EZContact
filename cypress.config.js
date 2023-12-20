0cd2e12ded93545951a78cf5212171e3b4754247
Muhammad Arshad <m.arshad2348@gmail.com>
2023-12-20 23:40:05 +0500

    Mochawsome Reporter Integrated

--- cypress.config.js
+++ cypress.config.js
@@ -5,9 +5,18 @@ module.exports = defineConfig({
   watchForFileChanges : false,
   chromeWebSecurity : false,
   retries:{runMode:1},
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'EZContact Test Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     baseUrl : 'https://www.ezcontacts.com',
     "defaultCommandTimeout": 20000,
